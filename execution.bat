dc2b4ab90dbd7317eafe30fb7415407928892b25
jignesh <jigneshchauhan7600@gmail.com>
2023-06-14 19:25:48 +0530

    execution.bat modified for jenkinsexecution by jignesh on 14 june 2023

--- execution.bat
+++ execution.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 start cmd /k "start /B java -jar selenium-server-4.9.1.jar hub --port 4444 && title Selenium_Grid_Hub" 
-start cmd /k "start /B java -Dwebdriver.chrome.driver=%~dp0\Drivers\chromedriver_v114.exe -jar selenium-server-4.9.1.jar node --port 5555 --host localhost && title Selenium_Grid_Node" 
+start cmd /k "start /B java -Dwebdriver.chrome.driver=Drivers\chromedriver_v114.exe -jar selenium-server-4.9.1.jar node --port 5555 --host localhost && title Selenium_Grid_Node" 
 start cmd /k "mvn clean test && mytaskkill.bat"
